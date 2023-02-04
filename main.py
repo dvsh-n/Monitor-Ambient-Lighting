1944ef1a52a4a41d6c493456c37800e1b556ace2
Devesh Nath <deveshnath101@gmail.com>
2023-02-04 15:16:45 -0700

    Screenshot and averaging

--- main.py
+++ main.py
@@ -2,8 +2,22 @@ import numpy as np
 import pyautogui
 import imutils
 import cv2
+from matplotlib import pyplot as plt
+import time
 
-image = pyautogui.screenshot()
-image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
-print(image.shape)
-cv2.imwrite("in_memory_to_disk.png", image)
+for i in range(10):
+    image = pyautogui.screenshot()
+    image_arr = np.array(image)
+    dims = image_arr.shape
+    avg_color_row = np.average(image_arr, axis=0)
+    avg_color  = np.average(avg_color_row, axis=0)
+    avg_color_img = np.ones((1920, 1080, 3), dtype=np.uint8)
+    avg_color_img[:,:] = avg_color 
+    plt.imshow(avg_color_img)
+    plt.axis('off')
+    plt.show()
+
+    time.sleep(3)
+    
+
+
