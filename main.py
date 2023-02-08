68780ef8f44a156d597eef7a8dc508e18d1b0cce
Devesh Nath <deveshnath101@gmail.com>
2023-02-08 15:44:33 -0700

    setting up the image processing function

--- main.py
+++ main.py
@@ -5,6 +5,27 @@ import cv2
 from matplotlib import pyplot as plt
 import time
 
+# PC screen dims = (1080,1920,3) 
+# Monitor Screen dims = (1440, 2560, 3)
+
+# Total number of LEDs
+top_bottom_leds = 10
+left_right_leds = 5
+
+def color_processing(top_bottom_leds, left_right_leds, image_arr):
+    top_colors = []
+    bottom_colors = []
+    left_colors = []
+    right_colors = []
+    (horizontal, vertical, RGB) = image_arr.shape
+    hor_square_dims = horizontal/top_bottom_leds
+    ver_square_dims = vertical/left_right_leds
+
+    for i in range(top_bottom_leds):
+        
+        image_slice = image_arr[]
+
+
 # for i in range(10):
 #     image = pyautogui.screenshot()
 #     image_arr = np.array(image)
