9164898e99f0bd2c3b5a020fa3dfa2542b3de640
Devesh Nath <deveshnath101@gmail.com>
2023-03-22 00:43:20 -0700

    added reordering in colors_from_img function

--- main.py
+++ main.py
@@ -36,34 +36,39 @@ def avg_color(image):
     return avg_color
 
 
-def colors_from_img(top_bottom_leds, left_right_leds, image, extra_slice = (0, 0)): # extra_slice = (# extra vert px for hor, # extra hor px for vert)
+def colors_from_img(top_bottom_leds, left_right_leds, image, extra_slice = (0, 0), order = [1,2,0,3]): # extra_slice = (# extra vert px for hor, # extra hor px for vert)
     colors = [[], [], [], []]# 0:top 1:bottom 2:left 3:right
 
     (vertical, horizontal, _) = image.shape
     hor_scn_sqr_dims = horizontal//top_bottom_leds 
     ver_scn_sqr_dims = vertical//left_right_leds + extra_slice[1]
 
+    top_idx = order.index(0) # 2
+    bottom_idx = order.index(1) # 0
+    left_idx = order.index(2) # 1
+    right_idx = order.index(3) # 3
+
     for i in range(top_bottom_leds):
         top_image_slice = image[0:(hor_scn_sqr_dims+extra_slice[0]), # vertical
                                     (i*hor_scn_sqr_dims):((i+1)*hor_scn_sqr_dims), #horizontal
                                     :]
-        colors[0].append(avg_color(top_image_slice))
+        colors[top_idx].append(avg_color(top_image_slice))
 
         bottom_image_slice = image[(vertical-(hor_scn_sqr_dims+extra_slice[0])):vertical, # vertical
                                         (i*hor_scn_sqr_dims):((i+1)*hor_scn_sqr_dims), # horizontal
                                         :]
-        colors[1].append(avg_color(bottom_image_slice))
+        colors[bottom_idx].append(avg_color(bottom_image_slice))
     
     for i in range(left_right_leds):
         left_image_slice = image[(i*ver_scn_sqr_dims):((i+1)*ver_scn_sqr_dims), # vertical 
                                     0:(ver_scn_sqr_dims+extra_slice[1]), # horizontal
                                     :]
-        colors[2].append(avg_color(left_image_slice))
+        colors[left_idx].append(avg_color(left_image_slice))
 
         right_image_slice = image[(i*ver_scn_sqr_dims):((i+1)*ver_scn_sqr_dims), # vertical 
                                         (horizontal-(ver_scn_sqr_dims+extra_slice[1])):horizontal, # horizontal
                                         :]
-        colors[3].append(avg_color(right_image_slice))
+        colors[right_idx].append(avg_color(right_image_slice))
 
     return colors
 
